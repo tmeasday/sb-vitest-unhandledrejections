e326fd52d0cffe19cd8d1ac8d1dac3ef5da74cd6
Tom Coleman <tom@chromatic.com>
2025-01-30 16:58:32 +1100

    Add helpful readme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -13,9 +13,10 @@ const preview: Preview = {
 
 export default preview;
 
-window.addEventListener("unhandledrejection", function (e) {
-  console.log("vvv -- Caught handled rejection -- vvv");
-  console.log(e.reason);
-  console.log(e.reason?.stack);
-  console.log("^^^ -- Done in caught unhandled rejection -- ^^^");
-});
+// Toggle this block to see a method to catch rejections and associate with the story
+// window.addEventListener("unhandledrejection", function (e) {
+//   console.log("vvv -- Caught handled rejection -- vvv");
+//   console.log(e.reason);
+//   console.log(e.reason?.stack);
+//   console.log("^^^ -- Done in caught unhandled rejection -- ^^^");
+// });
